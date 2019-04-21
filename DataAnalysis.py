44eb1005ba61b7ebd040e36b4676506821952bde
molychin <molychin@qq.com>
2019-04-21 23:23:25 +0800

    2019.04.21.23:23 molychin

---
+++ DataAnalysis.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 21 22:47:04 2019
+
+@author: Administrator
+"""
+
+##数据科学
+#Data science is a multi-disciplinary field that uses scientific methods, processes, 
+#algorithms and systems to extract knowledge and insights from data in various forms, 
+#both structured and unstructured, similar to data mining.
+#Data science is a "concept to unify statistics, data analysis, machine learning and their 
+#related methods" in order to "understand and analyze actual phenomena" with data.
+#数据科学是一个多学科领域，使用科学的方法、过程、算法和系统从数据中提取知识和见解，无论是结构化的
+#还是非结构化的，类似于数据挖掘。
+#数据科学是一个“统一统计、数据分析、机器学习及其相关方法的概念”，目的是利用数据“理解和分析实际现象”。
+#它采用了在数学、统计、信息科学和计算机科学的背景下从多个领域汲取的技术和理论。
+#
+##Data Analysis 数据分析
+#
+##■定义
+#Data analysis is a process of inspecting, cleansing, transforming, and modeling data 
+#with the goal of discovering useful information, informing conclusions, and supporting 
+#decision-making. Data analysis has multiple facets and approaches, encompassing 
+#diverse techniques under a variety of names, while being used in different business, 
+#science, and social science domains. 
+#In today's business, data analysis is playing a role in making decisions more scientific 
+#and helping the business achieve effective operation.
+#数据分析是一个检查、清理、转换和建模数据的过程，目的是发现有用的信息、得出结论和支持决策。数据分析
+#有多个方面和方法，包括各种名称下的各种技术，同时用于不同的商业、科学和社会科学领域。
+#在当今的企业中，数据分析在使决策更加科学和帮助企业实现有效运营方面发挥着作用。
+#
+#数据分析是指用适当的统计分析方法对收集来的大量数据进行分析，提取有用信息和形成结论而对数据加以详细研究和概括总结的过程。
+#
+##数据挖掘
+#Data mining is a particular data analysis technique that focuses on modeling and 
+#knowledge discovery for predictive rather than purely descriptive purposes, while 
+#business intelligence covers data analysis that relies heavily on aggregation, 
+#focusing mainly on business information.
+#数据挖掘是一种特殊的数据分析技术，它专注于建模和知识发现，以实现预测而非纯粹的描述性目的，而商务智
+#能则涵盖了严重依赖于聚合的数据分析
+#
+##■数据分析的几个阶段
+#●Data requirements  数据需求
+#
+#●Data collection   数据采集
+#数据是从各种来源的文集。分析师可以将需求传达给数据保管人，例如组织内的信息技术人员。还可以从环境中的
+#传感器（如交通摄像机、卫星、记录设备等）收集数据。还可以通过访谈、在线资源下载或阅读文档获取数据。
+#
+#
+#●Data processing   数据处理
+#
+#
+#
+#●Data cleaning    数据清洗
+#一旦处理和组织，数据可能不完整、包含重复或包含错误。由于数据输入和存储方式存在问题，因此需要进行数据清理。
+#数据清理是防止和纠正这些错误的过程。常见的任务包括记录匹配、识别数据的不准确、现有数据的总体质量、重复数
+#据消除和列分段。此类数据问题也可以通过各种分析技术来识别。例如，根据财务信息，可将特定变量的总数与独立公
+#布的可靠数字进行比较。也可审查高于或低于预先确定阈值的异常金额。有几种类型的数据清理取决于数据类型，如
+#电话号码、电子邮件地址、雇主等。可以使用离群值检测的定量数据方法来消除可能输入错误的数据。文本数据拼写
+#检查可以用来减少输入错误的单词数量，但是很难判断单词本身是否正确。
+#
+#
+#●Exploratory data analysis  探索性数据分析
+#
+#
+#●Modeling and algorithms  建模与算法
+#
+#
+#
+#●Data product  数据产品
+#
+#
+#
+#●Communication  通讯
+#Data visualization to understand the results of a data analysis.
+#
+#●
+#
+#
+#●
+#
+#
+#
+#●
+#
+#
+#●
+#
+#
+#●
+#
+#
+#
+#
+#
+
+
+
+
+
+
+
+
+
+
+
